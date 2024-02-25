2afa3f9436267a50582d310f3fb88c6e1cea4ce5
Bohdan11Dii <bodyabrovdiy@gmail.com>
2024-02-25 20:22:01 +0000

    Add models Categories and Color, registration this model in admin and create demo version in views file

--- product/models.py
+++ product/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=256)
+    
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from product.models import Category
+from product.serializers import CategorySerializer
+from rest_framework.views import APIView
 
-# Create your views here.
+# @api_view(["GET"])
+# def category_list(request):
+#     if request.method == "GET":
+        
+    
+
+class CategoryList(APIView):
+    def get(self, request) -> Response:
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data, status=200)
+    
