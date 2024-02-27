e58bb5da8771f1f6fdda70e27b1d13b489c2eba1
Bohdan11Dii <bodyabrovdiy@gmail.com>
2024-02-27 16:06:41 +0000

    Realize CategoryViewSet, CategorySerializer  and add swagger to urls

--- product/serializers.py
+++ product/serializers.py
@@ -2,13 +2,8 @@ from rest_framework import serializers
 from product.models import Category
 
 
-class CategorySerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=256, required=True)
+class CategorySerializer(serializers.ModelSerializer):
     
-    def create(self, validated_data):
-        return Category.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.save()
-        return instance
+    class Meta:
+        model = Category
+        fields = "__all__"
--- product/views.py
+++ product/views.py
@@ -1,19 +1,9 @@
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from product.models import Category
 from product.serializers import CategorySerializer
-from rest_framework.views import APIView
+from rest_framework import  viewsets
 
-# @api_view(["GET"])
-# def category_list(request):
-#     if request.method == "GET":
-        
-    
 
-class CategoryList(APIView):
-    def get(self, request) -> Response:
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data, status=200)
-    
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+ 
