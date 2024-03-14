5f0f2d1d47cedd5512c340b60cca541fc88055de
Bohdan11Dii <bodyabrovdiy@gmail.com>
2024-03-14 14:47:26 +0000

    Realize admin.TabularInline for model Product

--- product/admin.py
+++ product/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from product.models import Category, Color, Image, Product
 # Register your models here.
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline
+    ]
+
 admin.site.register(Category)
 admin.site.register(Color)
-admin.site.register(Image)
-admin.site.register(Product)
+
